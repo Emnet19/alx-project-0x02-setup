bed8d57174a0265169cd38314f8dba93f45e2b76
Emnet Befkadu <emnbef456@gmail.com>
2025-09-27 06:03:36 +0300

    link

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,9 +1,11 @@
     import React from 'react'
-    
+    import Link from 'next/link'
     const Header = () => {
       return (
         <div>
-          
+          Link to <Link href="/home">Home</Link>
+          <br />
+          Link to <Link href="/about">About</Link>
         </div>
       )
     }
