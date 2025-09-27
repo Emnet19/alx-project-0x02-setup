74c420a4b9f4ddf9da2f1a99d82f36fbf0ea7958
Emnet Befkadu <emnbef456@gmail.com>
2025-09-27 06:12:23 +0300

    button

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
+// pages/about.tsx
+import React from "react";
+import Button from "@/components/common/Button";
 
-const about = () => {
+const About: React.FC = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <main className="p-6 space-y-4">
+      <h1 className="text-2xl font-bold mb-4">About Page</h1>
 
-export default about
+      <div className="flex gap-4">
+        <Button label="Small Button" size="small" shape="rounded-sm" />
+        <Button label="Medium Button" size="medium" shape="rounded-md" />
+        <Button label="Large Button" size="large" shape="rounded-full" />
+      </div>
+    </main>
+  );
+};
 
+export default About;
